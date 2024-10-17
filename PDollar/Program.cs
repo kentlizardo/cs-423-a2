167aaeaf8dbc8259b66b57a9f06c64ce1821f97c
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-10-16 20:37:23 -0500

    Use pattern matching for simple command parsing. Take out hello world statement

--- PDollar/Program.cs
+++ PDollar/Program.cs
@@ -4,10 +4,30 @@ namespace PDollar
 {
     internal class Program
     {
+        const string HelpText = """
+            pdollar
+              Prints a help screen with list of commands
+            pdollar -t <gesturefile>
+              Adds a gesture file to list of gesture templates.
+            pdollar -r
+              Resets all templates
+            pdollar <eventstream>
+              Reads in an eventstream file, printing any recognized gestures.
+            """;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            //PointCloudRecognizer.Classify();
+            switch(args)
+            {
+                case []:
+                    Console.WriteLine(HelpText);
+                    break;
+                default:
+                    Console.WriteLine("Invalid number or set of arguments. Use \"pdollar\" for help.");
+                    break;
+            }
+
+            //string[] x =  File.ReadAllLines();
         }
     }
 }
